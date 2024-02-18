3d6fa396fe19fb1c85c2e6a221d8760f7ba26c0c
Mickey2004 <44784481+Pavel-Shcherbo@users.noreply.github.com>
2024-02-19 02:19:20 +0300

    1

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,5 @@ Bsrc/main/java/org/date/dateconverter/DateConverterApplication.java,9/6/9665bbc
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 4
 mvnw,5/d/5d837c4ed85ecaaf932c506e80ff5d7b9f3d590d
+w
+Gsrc/test/java/org/date/dateconverter/DateConverterApplicationTests.java,2/b/2b66dc608cc3584e676b7e6faea7059de5449c4e
--- src/test/java/org/date/dateconverter/DateConverterApplicationTests.java
+++ src/test/java/org/date/dateconverter/DateConverterApplicationTests.java
@@ -2,13 +2,14 @@ package org.date.dateconverter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DateConverterApplicationTests {
 
     @Test
       void contextLoads() {
-
+        assertTrue(true);
     }
 
 }
